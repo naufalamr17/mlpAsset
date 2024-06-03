5ab48115fa13918353fe5241db8c6e19a4c094ed
Irfan Maulana <irfanmaulana@ina17.id>
2024-06-03 11:26:17 +0700

    Update: fix layout login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,17 +15,22 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <div class="flex justify-center mb-4">
-                <img src="{{ asset('img/mlpLogo.jpg') }}" alt="Application Logo" class="w-20 h-20 fill-current text-gray-500" />
+<body class="font-sans text-gray-900 antialiased bg-gray-100">
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="flex flex-row bg-white shadow-md rounded-lg overflow-hidden w-full sm:max-w-6xl min-h-[70vh]">
+            <div class="flex flex-col items-start justify-center pl-6 ml-24 sm:pl-10 p-10 sm:w-1/2 bg-white"> <img src="{{ asset('img/mlpLogo.jpg') }}" alt="Application Logo" class="w-48 h-48 fill-current text-gray-500" />
             </div>
-            <h1 class="text-3xl font-bold leading-tight text-center">Log in to your account</h1>
-            <p class="text-gray-400 mt-2 mb-4 text-center">Welcome back! Please enter your details.</p>
-            {{ $slot }}
+            <div class="w-full mr-10 mt-11 mb-11 sm:w-1/2 px-6 py-4">
+                <div class="flex justify-center mb-4">
+                    <img src="{{ asset('img/mlpLogo.jpg') }}" alt="Application Logo" class="w-20 h-20 fill-current text-gray-500" />
+                </div>
+                <h1 class="text-3xl font-bold leading-tight text-center">Log in to your account</h1>
+                <p class="text-gray-400 mt-2 mb-4 text-center">Welcome back! Please enter your details.</p>
+                {{ $slot }}
+            </div>
         </div>
     </div>
 </body>
+
 
 </html>
