5c226373f7343296a9b2a13831365a308af8a98c
naufalamr17 <naufal.mtsyurja91@gmail.com>
2024-06-03 13:06:20 +0700

    Add: Add dropdown in sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,32 @@
                         </x-nav-link>
                     </li>
                     <li class="mt-4">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard!!')">
-                            {{ __('Inventory Asset') }}
-                        </x-nav-link>
+                        <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+                            <div @click="open = ! open">
+                                <button class="inline-flex items-center px-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Inventory Asset</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </div>
+                                </button>
+                            </div>
+
+                            <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" class="absolute z-50 mt-2 w-48 rounded-md shadow-lg ltr:origin-top-left rtl:origin-top-right start-0" style="display: none;" @click="open = false">
+                                <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
+                                    <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/profile">Profile</a>
+
+                                    <!-- Authentication -->
+                                    <form method="POST" action="http://127.0.0.1:8000/logout">
+                                        <input type="hidden" name="_token" value="iMsqjh3mlAvGKl4pXgc4k6GvJeKRz96CXmYwQC1E" autocomplete="off">
+                                        <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault();
+                                                this.closest('form').submit();">Log Out</a>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </li>
                     <li class="mt-4">
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard!!')">
