363d0387f4b6140d8466009966eabff3dc8884b6
naufalamr17 <naufal.mtsyurja91@gmail.com>
2024-06-03 13:12:41 +0700

    Update: Fix dropdown sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,14 +51,12 @@
 
                             <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" class="absolute z-50 mt-2 w-48 rounded-md shadow-lg ltr:origin-top-left rtl:origin-top-right start-0" style="display: none;" @click="open = false">
                                 <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
-                                    <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/profile">Profile</a>
-
-                                    <!-- Authentication -->
-                                    <form method="POST" action="http://127.0.0.1:8000/logout">
-                                        <input type="hidden" name="_token" value="iMsqjh3mlAvGKl4pXgc4k6GvJeKRz96CXmYwQC1E" autocomplete="off">
-                                        <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault();
-                                                this.closest('form').submit();">Log Out</a>
-                                    </form>
+                                    <div class="px-3 py-2"><x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard!!')">
+                                            {{ __('Input Asset') }}
+                                        </x-nav-link></a></div>
+                                    <div class="px-3 py-2"><x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard!!')">
+                                            {{ __('Dude Asset') }}
+                                        </x-nav-link></a></div>
                                 </div>
                             </div>
                         </div>
